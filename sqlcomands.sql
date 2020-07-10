3086dcb7c9c0386e44fed16cc15ec1ee486073fa
pawan2014 <pawan2014@users.noreply.github.com>
2020-07-10 13:28:04 -0500

    Update sqlcomands.sql

--- sqlcomands.sql
+++ sqlcomands.sql
@@ -1,3 +1,20 @@
 select count(1)
 from events
 where time between '2018-01-01' and '2018-01-31'
+
+
+
+Find rows created within the last week:
+select count(1)
+from events
+where time > now() - interval '1 week'; -- or '1 week'::interval, as you like
+
+
+Find rows created between one and two weeks ago:
+
+
+select count(1)
+from events
+where time between (now() - '1 week'::interval) and (now() - '2 weeks'::interval);
+
+https://popsql.com/learn-sql/postgresql/how-to-query-date-and-time-in-postgresql
